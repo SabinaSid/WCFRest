30f55619a84a413940a7be3c7ea53cfda4c891af
Sabina <kaiser2169@gmail.com>
2020-11-08 19:38:51 +0300

    Добавьте файлы проекта.

---
+++ WCFRest/BlogService.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFRest
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "BlogService" в коде, SVC-файле и файле конфигурации.
+    // ПРИМЕЧАНИЕ. Чтобы запустить клиент проверки WCF для тестирования службы, выберите элементы BlogService.svc или BlogService.svc.cs в обозревателе решений и начните отладку.
+    public class BlogService : IBlogService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
