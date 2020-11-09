1483df39366a7fed9e3691435e320426d92418d9
Sabina <kaiser2169@gmail.com>
2020-11-09 11:02:20 +0300

    CRUD PostTypes  реализация

--- WCFRest/BlogService.svc.cs
+++ WCFRest/BlogService.svc.cs
@@ -11,8 +11,49 @@ namespace WCFRest
     // ПРИМЕЧАНИЕ. Чтобы запустить клиент проверки WCF для тестирования службы, выберите элементы BlogService.svc или BlogService.svc.cs в обозревателе решений и начните отладку.
     public class BlogService : IBlogService
     {
+        public int AddPostType(string postTypeName)
+        {
+            var datacontext = new DataClassesDataContext();
+            var pt = new PostType();
+            pt.PostTypeName = postTypeName;
+            datacontext.PostType.InsertOnSubmit(pt);
+            datacontext.SubmitChanges();
+            return pt.PostTypeID;
+
+        }
+
+        public void DeletePostType(string postTypeId)
+        {
+            var datacontext = new DataClassesDataContext();
+            var pt = datacontext.PostType.FirstOrDefault(x => x.PostTypeID.ToString() == postTypeId);
+            datacontext.PostType.DeleteOnSubmit(pt);
+            datacontext.SubmitChanges();
+        }
+
         public void DoWork()
         {
+        }
+
+        public PostType GetPostType(string postTypeId)
+        {
+            var datacontext = new DataClassesDataContext();
+            return datacontext.PostType.FirstOrDefault(x => x.PostTypeID.ToString() == postTypeId);
+
+        }
+
+        public IEnumerable<PostType> GetPostTypes()
+        {
+            var datacontext = new DataClassesDataContext();
+            return datacontext.PostType.AsEnumerable();
+
+        }
+
+        public void UpdatePostType(string postTypeId, string postTypeName)
+        {
+            var datacontext = new DataClassesDataContext();
+            var pt = datacontext.PostType.FirstOrDefault(x => x.PostTypeID.ToString() == postTypeId);
+            pt.PostTypeName = postTypeName;
+            datacontext.SubmitChanges();
         }
     }
 }
--- WCFRest/IBlogService.cs
+++ WCFRest/IBlogService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
+using System.ServiceModel.Web;
 using System.Text;
 
 namespace WCFRest
@@ -19,6 +20,7 @@ namespace WCFRest
         /// </summary>
         /// <returns>таблица PostType</returns>
         [OperationContract]
+        [WebGet(UriTemplate = "/PostTypes", ResponseFormat = WebMessageFormat.Json)]
         IEnumerable<PostType> GetPostTypes();
 
         /// <summary>
@@ -27,7 +29,8 @@ namespace WCFRest
         /// <param name="postTypeId"></param>
         /// <returns>запись из таблицы PostType</returns>
         [OperationContract]
-        IEnumerable<PostType> GetPostType(string postTypeId);
+        [WebGet(UriTemplate = "/PostTypes/{postTypeId}", ResponseFormat = WebMessageFormat.Json)]
+        PostType GetPostType(string postTypeId);
 
         /// <summary>
         /// Метод добавления записи в талблице PostType
@@ -35,21 +38,22 @@ namespace WCFRest
         /// <param name="postTypeId">идентификатор записи</param>
         /// <returns>идентификатор новой записи в таблице PostType</returns>
         [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "/PostTypeAdd/{postTypeName}", ResponseFormat = WebMessageFormat.Json)]
         int AddPostType(string postTypeName);
-
         /// <summary>
         /// Метод редактирования записи в таблице PostType
         /// </summary>
         /// <param name="postTypeId">идентификатор записи</param>
         /// <param name="postTypeName">новое значение типа поста</param>
         [OperationContract]
+        [WebInvoke(Method = "PUT", UriTemplate = "/PostTypeUpdate/{postTypeId}/{postTypeName}", ResponseFormat = WebMessageFormat.Json)]
         void UpdatePostType(string postTypeId, string postTypeName);
-
         /// <summary>
         /// Метод удаления записи из таблицы PostType
         /// </summary>
         /// <param name="postTypeId">идентификатор записи</param>
         [OperationContract]
+        [WebInvoke(Method = "DELETE", UriTemplate = "/PostTypeDelete/{postTypeId}", ResponseFormat = WebMessageFormat.Json)]
         void DeletePostType(string postTypeId);
 
 
