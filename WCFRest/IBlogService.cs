9a0db86b93dc7bdf287fbe265c61e539e3931b77
Sabina <kaiser2169@gmail.com>
2020-11-09 13:10:57 +0300

    CRUD for Post

--- WCFRest/IBlogService.cs
+++ WCFRest/IBlogService.cs
@@ -40,6 +40,7 @@ namespace WCFRest
         [OperationContract]
         [WebInvoke(Method = "POST", UriTemplate = "/PostTypeAdd/{postTypeName}", ResponseFormat = WebMessageFormat.Json)]
         int AddPostType(string postTypeName);
+
         /// <summary>
         /// Метод редактирования записи в таблице PostType
         /// </summary>
@@ -48,6 +49,7 @@ namespace WCFRest
         [OperationContract]
         [WebInvoke(Method = "PUT", UriTemplate = "/PostTypeUpdate/{postTypeId}/{postTypeName}", ResponseFormat = WebMessageFormat.Json)]
         void UpdatePostType(string postTypeId, string postTypeName);
+
         /// <summary>
         /// Метод удаления записи из таблицы PostType
         /// </summary>
@@ -55,6 +57,53 @@ namespace WCFRest
         [OperationContract]
         [WebInvoke(Method = "DELETE", UriTemplate = "/PostTypeDelete/{postTypeId}", ResponseFormat = WebMessageFormat.Json)]
         void DeletePostType(string postTypeId);
+
+        /// <summary>
+        /// Метод получения записей из таблицы Post
+        /// </summary>
+        /// <returns>записи таблицы Post</returns>
+        [OperationContract]
+        [WebInvoke(Method ="GET",UriTemplate ="/Posts",RequestFormat =WebMessageFormat.Json)]
+        IEnumerable<Post> GetPosts();
+
+        /// <summary>
+        /// Метод получения записи таблицы Post
+        /// </summary>
+        /// <param name="postId">идентификатор записи</param>
+        /// <returns>запись таблицы Post</returns>
+        [OperationContract]
+        [WebInvoke(Method ="GET",UriTemplate ="/Posts/{postId}",RequestFormat =WebMessageFormat.Json)]
+        Post GetPost(string postId);
+
+        /// <summary>
+        /// Метод добавления записи в таблицу Post
+        /// </summary>
+        /// <param name="postHeader">заголовок поста</param>
+        /// <param name="postText">текст поста</param>
+        /// <returns></returns>
+        [OperationContract]
+        [WebInvoke(Method ="POST",UriTemplate ="/PostAdd/{postHeader}/{postText}",RequestFormat =WebMessageFormat.Json)]
+        int AddPost(string postHeader, string postText);
+
+        /// <summary>
+        /// Метод редактирования записи в таблице Post
+        /// </summary>
+        /// <param name="postId">идентификатор записи</param>
+        /// <param name="postHeader">заголовок поста</param>
+        /// <param name="postText">текст поста</param>
+        /// <returns></returns>
+        [OperationContract]
+        [WebInvoke(Method ="PUT",UriTemplate ="/PostUpdate/{postId}/{postHeader}/{postText}")]
+        int UpdatePost(string postId, string postHeader, string postText);
+
+        /// <summary>
+        /// Метод удаления записи в таблице Post
+        /// </summary>
+        /// <param name="postId">идентификатор записи</param>
+        /// <returns></returns>
+        [OperationContract]
+        [WebInvoke(Method ="DELETE",UriTemplate ="/PostDelete/{postId}")]
+        int DeletePost(string postId);
 
 
 
